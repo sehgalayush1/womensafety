5cb9a7658daa47f6774ffda673bd02f64f784b5d
Ayush Sehgal <ayushsehgal@ayush.local>
2017-03-04 15:04:22 +0530

    organized structure

--- wsafe/app/models.py
+++ wsafe/app/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Contacts(models.Model):
+	users = models.ManyToManyField(User)
+	name = models.CharField(max_length=200)
+	email = models.CharField(max_length=100)
+	number = models.CharField(max_length=10)
